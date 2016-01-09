bc2f5da9b7240fc07d1e73548a69f8b23151bd9b
H. Dale McBane <h.dale.mcbane@gmail.com>
2016-01-08 20:25:40 -0500

    Take word guess out because it's not complete...

--- launcher.py
+++ launcher.py
@@ -13,24 +13,24 @@ import squirrel
 import starpusher
 import tetromino
 import tetrominoforidiots
-import wordguess
+# import wordguess
 import wormy
 
-games = [[flippy.main, "A game like Reversi"],
+games = [[gemgem.main, "A game like Bejeweled"],
          [fourinarow.main, "A game like Connect Four"],
-         [gemgem.main, "A game like Bejeweled"],
-         [starpusher.main, "A game like Sokoban"],
+         [flippy.main, "A game like Reversi"],
          [simulate.main, "A game like Simon"],
          [wormy.main, "A game like Snake"],
+         [starpusher.main, "A game like Sokoban"],
          [tetromino.main, "A game like Tetris"],
          [tetrominoforidiots.main, "Tetris for Idiots"],
          [pentomino.main, "5 block Tetris"],
+         [squirrel.main, "Eat the smaller squirrels"],
          [inkspill.main, "Flood the screen with pixels"],
          [memorypuzzle.main, "Large memory puzzle"],
          [marcelmemory.main, "Memory puzzle with Marcel settings"],
-         [slidepuzzle.main, "Traditional slide puzzle"],
-         [squirrel.main, "Eat the smaller squirrels"],
-         [wordguess.main, "New word game"]]
+         [slidepuzzle.main, "Traditional slide puzzle"]]  # ,
+# [wordguess.main, "New word game"]]
 
 
 def showGames():
